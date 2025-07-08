6b2616d902093d801d9534ebe8f8e96d12cfb58d
Andreashptra <andreasahputrap@gmail.com>
2025-07-08 20:08:47 +0700

    Initial commit

---
+++ resources/views/admin/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<div class="py-6 px-6 text-center">
+    <p class="mb-0 fs-4">Website Portal Desa Sukorejo Copyright 2025</p> Repost By : <a href="https://www.instagram.com/andreashptr/?igsh=dHFzMW9raXlmdjY1&utm_source=qr#">AndreaSahputra</a>
+  </div>
